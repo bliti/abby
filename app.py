1bffa2074858603a8752a697143631dbc1fb6928
bliti <pablo.rivera.programmer@gmail.com>
2014-12-19 09:04:40 -0400

    push inicial

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, redirect
+from settings import DEBUG, URLS
+
+
+app = Flask(__name__)
+app.debug = DEBUG
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+#@app.route('/blog', methods=['GET'])
+#def blog():
+#    return redirect(URLS['blog'])
+
+
+#@app.route('/twitter', methods=['GET'])
+#def twitter():
+#    return redirect(URLS['twitter'])
+
+
+#@app.route('/youtube', methods=['GET'])
+#def youtube():
+#    return redirect(URLS['youtube'])
+
+
+#@app.route('/store', methods=['GET'])
+#def store():
+#    return redirect(URLS['store'])
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ settings.py
@@ -0,0 +1,15 @@
+try:
+    from development_settings import DEBUG
+except ImportError:
+    DEBUG = False
+
+
+try:
+    from development_settings import URLS
+except ImportError:
+    URLS = {
+            'blog': 'http://blog.wutbot.com',
+            'twitter': 'https://twitter.com/wutbot5000',
+            'youtube': 'https://www.youtube.com/channel/UCOlSfiGQCT5_rmcxQDNygkg',
+            'store': 'http://tindie.com' 
+            }
